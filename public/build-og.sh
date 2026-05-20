dc2d9da99c6ffe8f0bbdf510e37647eab4a36101
Hugh Pyle <hughpyle@gmail.com>
2026-05-20 00:44:19 -0700

    svg

--- public/build-og.sh
+++ public/build-og.sh
@@ -4,6 +4,6 @@ set -eu
 cd "$(dirname "$0")"
 
 for name in woah-og woah-slim; do
-  rsvg-convert -w 1200 -h 630 "$name.svg" -o "$name.png"
+  rsvg-convert -w 1200 "$name.svg" -o "$name.png"
   echo "wrote $name.png"
 done
